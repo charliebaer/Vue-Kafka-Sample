643062d509a0715166fa3d51e580d6eda44b2b23
charliebaer <sanjaypaulcharles98@gmail.com>
2020-10-04 16:46:58 +0530

    wrapped the producer in function as a module

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    
   </div>
+  <div id="app-5">
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+  </div>
+  <div id="app">
+      <p>{{ message }}</p>
+      <button v-on:click="reverseMessage">Reverse Message</button>
+      <p>
+          <input v-model="kafmessage" placeholder="Enter Kafka Message" />
+          <button v-on:click="sendkafmessagefunction">Send Message</button>
+          <p>Message is: {{ kafmessage }}</p>
+      </p>
+      
+    </div>
 </template>
 
 <script>
+var kaf = require("./producer.js");
+
+var Kafka = kaf();
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data: function () {
+    return {
+      message: "Hello Vue.js!",
+      kafmessage: "kaf-message",
+    };
+  },
+
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split("").reverse().join("");
+    },
+    sendkafmessage: function () {},
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
