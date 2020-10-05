2eb486e35343a3accf25df98642dfba126119006
charliebaer <sanjaypaulcharles98@gmail.com>
2020-10-06 00:21:08 +0530

    configured axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,64 +2,69 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
   </div>
+  <!--
   <div id="app-5">
     <p>{{ message }}</p>
     <button v-on:click="reverseMessage">Reverse Message</button>
   </div>
+  -->
   <div id="app">
-    <p>{{ message }}</p>
-    <button v-on:click="reverseMessage">Reverse Message</button>
+   <!-- <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button>              -->
+    
     <p></p>
     <input v-model="kafmessage" placeholder="Enter Kafka Message" />
     <button v-on:click="sendkafmessage">Send Message</button>
   </div>
 </template>
 
+
 <script>
-  export default {
-    name: "HelloWorld",
-    props: {
-      msg: String,
+import axios from "axios";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {
+      message: "Hello Vue.js!",
+      kafmessage: "kaf-message",
+    };
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split("").reverse().join("");
     },
-    data: function () {
-      return {
-        message: "Hello Vue.js!",
-        kafmessage: "kaf-message",
-      };
+    sendkafmessage: function () {
+      // var kafevent = this.kafmessage;
+      var message = this.kafmessage;
+      alert(message);
+      axios.post(`http://localhost:3000/${message}`, {
+        body: this.postBody,
+      });
     },
-    methods: {
-      reverseMessage: function () {
-        this.message = this.message.split("").reverse().join("");
-      },
-      sendkafmessage: function () {
-        // var kafevent = this.kafmessage;
-        var message = this.kafmessage
-        alert(message);
-        axios.post(`http://localhost:3000/${message}`, {
-          body: this.postBody
-        })
-      },
-    },
-  };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
+h3 {
+  margin: 40px 0 0;
+}
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-  a {
-    color: #42b983;
-  }
+a {
+  color: #42b983;
+}
 </style>
