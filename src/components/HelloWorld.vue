aed75ec78698eda0f414bc3353cd9ed777102c34
sanjay <sanjaypaulcharles98@gmail.com>
2020-10-06 15:29:19 +0530

    fixed query params in http requests

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,62 +9,79 @@
   </div>
   -->
   <div id="app">
-   <!-- <p>{{ message }}</p>
+    <!-- <p>{{ message }}</p>
     <button v-on:click="reverseMessage">Reverse Message</button>              -->
-    
+
     <p></p>
     <input v-model="kafmessage" placeholder="Enter Kafka Message" />
     <button v-on:click="sendkafmessage">Send Message</button>
+    <!--   <button v-on:click="postfunction">Send Message</button> -->
   </div>
 </template>
 
 
 <script>
-import axios from "axios";
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-  data: function () {
-    return {
-      message: "Hello Vue.js!",
-      kafmessage: "kaf-message",
-    };
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split("").reverse().join("");
+  import axios from "axios";
+  export default {
+    name: "HelloWorld",
+    props: {
+      msg: String,
     },
-    sendkafmessage: function () {
+    data: function () {
+      return {
+        message: "Hello Vue.js!",
+        kafmessage: null,
+        info: null  //delete this garbage!!!!
+      };
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split("").reverse().join("");
+      },
+
+
+      sendkafmessage: function () {
+        // var kafevent = this.kafmessage;        //don't delete function
+        var message = this.kafmessage;
+        alert(message);
+        axios
+          .post(`http://localhost:3000/${message}`);
+      },
+
+
+    },
+    /*
+    postfunction: function () {
       // var kafevent = this.kafmessage;
-      var message = this.kafmessage;
+      axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.info = response.data.bpi))
+        .catch(error => console.log(error));
+      var message = this.info;
       alert(message);
-      axios.post(`http://localhost:3000/${message}`, {
-        body: this.postBody,
-      });
+
     },
-  },
-};
+    */
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
